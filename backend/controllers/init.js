8ed822b46f704e915bbbad68f406ed1fe9a8ce5a
apple <apple@192.168.1.7>
2025-07-02 02:42:28 +0530

    init logic implemented

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require("fs").promises; //fs is file system comes with node, and promises is a utility which helps to create a file 
+const path = require("path");
+
 async function initRepo(){
-    console.log("Initialization of Repository is called");
+    const repoPath = path.resolve(process.cwd(), ".nest");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try{
+        await fs.mkdir(repoPath,{recursive: true});
+        await fs.mkdir(commitsPath,{recursive: true});
+        await fs.writeFile(
+            path.join(repoPath,"config.json"),
+            JSON.stringify({bucket: process.env.S3_BUCKET})
+        );
+        console.log("Repository initialized!!");
+    }catch(err){
+        res.error("Error in initialising repository: ", err);
+    }
 }
 
 module.exports = {initRepo};
