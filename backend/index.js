4afe69e6fd21f418c335fb92a835ef4ef660fcba
Eagleeye1811 <vedgawali@gmail.com>
2025-07-12 02:49:02 +0530

    Server logic created

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const http = require("http");
+const {Server} = require("socket.io");
+dotenv.config();
+
 const yargs = require("yargs");
 const {hideBin} = require("yargs/helpers");
 const {initRepo} = require("./controllers/init");
@@ -8,6 +17,7 @@ const {pushRepo} = require("./controllers/push");
 const {revertRepo} = require("./controllers/revert");
 
 yargs(hideBin(process.argv))
+    .command("start","Starts a new server",{},startServer)
     .command("init","Initialise a new repository",{},initRepo)
     .command("add <file>","Add a file to the repository",
         (yargs) => {
@@ -34,12 +44,84 @@ yargs(hideBin(process.argv))
     )
     .command("push", "Push commits to s3",{},pushRepo)
     .command("pull", "Pull commits to s3",{},pullRepo)
-    .command("revert <commitID>","Revert to a specific commit",
+    .command(
+        "revert <commitID>",
+        "Revert to a specific commit",
         (yargs) => {
-            yargs.positional("commitID",{
-                describe: "Commit ID to revert to",
-                type:"string",
-            })
-        },revertRepo)
+          yargs.positional("commitID", {
+            describe: "Comit ID to revert to",
+            type: "string",
+          });
+        },
+        (argv) => {
+          revertRepo(argv.commitID);
+        }
+      )
     .demandCommand(1,"You need at least one command")
     .help().argv;
+
+function startServer(){
+    const app = express();
+    const PORT = process.env.PORT || 8080;
+
+    app.use(bodyParser.json());
+    app.use(express.json());
+    app.use(cors({origin:'*'}));
+
+    const MONGODB_URI = process.env.MONGODB_URI;
+
+    const connectDB = async () => {
+        try{
+            const conn = await mongoose.connect(MONGODB_URI, {
+                useNewUrlParser:true,
+                useUnifiedTopology:true,
+            });
+            console.log(`MongoDB Connected: ${conn.connection.host}`);
+        }catch(err){
+            console.error(`MongoDB Connection Error: ${err.message}`);
+            process.exit(1);
+        }
+    }
+
+    connectDB();
+
+    app.get("/",(req,res) => {
+        res.send("Welcome to our website!!");
+    })
+
+    const httpServer = http.createServer(app);
+    const io = new Server(httpServer,{
+        cors: {
+            origin: '*',
+            methods: ['GET','POST'],
+        }
+    });
+
+    //trial purpose - remove after
+    user = "test";
+
+    io.on("connection",(socket)=>{
+        socket.on("joinRoom",(userId) => {
+            user = userId;
+            console.log("====");
+            console.log(user);
+            console.log("====");
+            socket.join(userId);
+        })
+    })
+
+    const db = mongoose.connection;
+
+    db.once("open", async () => {
+        console.log("CRUD Operations Called");
+        //CRUD Opeartions
+    })
+
+    httpServer.listen(PORT,() => {
+        console.log(`Server is running on ${PORT}`);
+    })
+}
+
+
+
+
